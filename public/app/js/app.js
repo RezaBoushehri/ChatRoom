fb0d25422ea95c83c793da6df72c96d7b08c8559
RezaBoushehri <reza.boshehry@gmail.com>
2024-11-19 18:04:48 +0330

    ss

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,9 +1,9 @@
 var App = angular.module('ChatRoom',['ngResource','ngRoute','ngStorage','socket.io','ngFileUpload','Controllers','Services'])
 .run(["$rootScope", function ($rootScope){
-	$rootScope.baseUrl = 'http://chat.systenics.com'; //Application URL
+	$rootScope.baseUrl = 'http://172.16.28.166:8282'; //Application URL
 }]);
 App.config(function ($routeProvider, $socketProvider){
-	$socketProvider.setConnectionUrl('http://chat.systenics.com'); // Socket URL
+	$socketProvider.setConnectionUrl('http://172.16.28.166:8282'); // Socket URL
 
 	$routeProvider	// AngularJS Routes
 	.when('/v1/', {
